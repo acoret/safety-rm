be63a9f76acdfae08bbad526a16374dccebff407
acoret <acoret@126.com>
2016-09-26 04:18:41 +0000

    damn it ahhhh!

--- IDtoname.lua
+++ IDtoname.lua
@@ -6,20 +6,22 @@ fuc.IDtoON=function (args)-- get the old path
 		   print "ID don't exsit,error 2"
 	   end
    	end
-	local name={},filename={},work,trashname
-	for i,v in pairs(file) do
+	local name={},filename={},work,tfpath,tryname
+	for i,v in pairs(file) do --insert time and try more
 	   work=v[3].." "
 	   work=string.gsub(work,"\"*-\"",function (x) table.insert(name,{v[2],x}) return nil end)
 	   work=string.gsub(work,"'*-'",function (x) table.insert(name,{v[2],x}) return nil end)
 	   work=string.gsub(work,"[^ ]- ",function (x) table.insert(name,{v[2],x}) return nil end)
 	   end
 	   for i,v in pairs(name) do
+		tryname=string.match(v[2],".+/(.-$)")
+		if not tryname then tryname=v[2] end
 	     if string.sub(v[2],1,1)=="/" then --v[2]=real filename v[3]=trashname
 		   --real path
-		 table.insert(filename,{v[2],v[3]})
+		 table.insert(filename,{v[2],tfpath})
    	     else
 		   --false path
-		 table.insert(filename,{v[1].. v[2],v[3]})
+		 table.insert(filename,{v[1].. v[2],tfpath})
 	     end
 	   end
 	   return filename;
