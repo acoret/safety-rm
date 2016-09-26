c21edd906ec0075cf969402ab997217f4bde2c63
acoret <acoret@126.com>
2016-09-26 11:10:12 +0800

    Add files via upload
    fix some bug and some improvement

--- IDtoname.lua
+++ IDtoname.lua
@@ -1,4 +1,4 @@
-fuc.IDtoON=function (args)-- get the old name
+fuc.IDtoOP=function (args)-- get the old path
 	local file={}
 	setmetatable(file,{__index=table})
 	for i=2,#args do
@@ -6,20 +6,20 @@ fuc.IDtoON=function (args)-- get the old name
 		   print "ID don't exsit,error 2"
 	   end
    	end
-	local name={},filename={}
+	local name={},filename={},work
 	for i,v in pairs(file) do
-	   v[3]=v[3].." "
-	   v[3]=string.gsub(v[3],"\"*-\"",function (x) table.insert({v[2],name}) return nil end)
-	   v[3]=string.gsub(v[3],"'*-'",function (x) table.insert({v[2],name}) return nil end)
-	   v[3]=string.gsub(v[3],"[^ ]- ",function (x) table.insert({v[2],name}) return nil end)
+	   work=v[3].." "
+	   work=string.gsub(work,"\"*-\"",function (x) table.insert(name,{v[2],x}) return nil end)
+	   work=string.gsub(work,"'*-'",function (x) table.insert(name,{v[2],x}) return nil end)
+	   work=string.gsub(work,"[^ ]- ",function (x) table.insert(name,{v[2],x}) return nil end)
 	   end
 	   for i,v in pairs(name) do
 	     if string.sub(v[2],1,1)=="/" then
 		   --real path
-		 table.insert(filename,v[2])
+		 table.insert(filename,{v[2],v[2]})
    	     else
 		   --false path
-		 table.insert(filename,v[1].. v[2])
+		 table.insert(filename,{v[1].. v[2],v[2]})
 	     end
 	   end
 	   return filename;
